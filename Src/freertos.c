9dd721b66ffb1f145d7d52de580e6ea1de1ae9cc
Robert Galat <robgal519@gmail.com>
2020-06-26 16:57:14 +0200

    fix issue with endpoint detection

--- Src/freertos.c
+++ Src/freertos.c
@@ -1,34 +1,36 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * File Name          : freertos.c
-  * Description        : Code for freertos applications
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
 #include "FreeRTOS.h"
-#include "cmsis_os2.h"
-#include "task.h"
-#include "main.h"
 #include "cmsis_os.h"
+#include "cmsis_os2.h"
+#include "main.h"
+#include "task.h"
+
 
 /* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */     
-#include "lwip.h"
+/* USER CODE BEGIN Includes */
 #include "httpserver-netconn.h"
+#include "lwip.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -53,14 +55,13 @@
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 1024 * 8
-};
+    .name = "defaultTask",
+    .priority = (osPriority_t)osPriorityNormal,
+    .stack_size = 1024 * 8};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-   
+
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
@@ -69,13 +70,13 @@ extern void MX_LWIP_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
+ * @brief  FreeRTOS initialization
+ * @param  None
+ * @retval None
+ */
 void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
-       
+
   /* USER CODE END Init */
 
   /* USER CODE BEGIN RTOS_MUTEX */
@@ -96,33 +97,52 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the thread(s) */
   /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  defaultTaskHandle =
+      osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
+}
 
+#define HTTP_OK "HTTP/1.1 200 OK\n"
+#define HTTP_NOT_FOUND "HTTP/1.1 404 Not Found\n"
+
+#define CONTENT_TYPE_JSON "Content-Type: application/json; charset=utf-8\n"
+#define CONTENT_TYPE_TEXT "Content-Type: text/html; charset=utf-8\n"
+
+#define END_OF_HEADER "\r\n"
+#include "../webpages/index.h"
+#include <string.h>
+err_t index_html_handler(struct netconn *connection_context) {
+  err_t ret = netconn_write(
+      connection_context, HTTP_OK CONTENT_TYPE_TEXT "\r\n",
+      strlen(HTTP_OK) + strlen(CONTENT_TYPE_TEXT) + 2, NETCONN_NOCOPY);
+
+  ret = netconn_write(connection_context, (const unsigned char *)index_html,
+                      index_html_len, NETCONN_NOCOPY);
+  return ret;
 }
 
 /* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used 
-  * @retval None
-  */
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
+void StartDefaultTask(void *argument) {
   log_message("start default task\n");
   /* init code for LWIP */
   MX_LWIP_Init();
   /* USER CODE BEGIN StartDefaultTask */
   register_endpoint(GET, "/alive", alive_handler);
+  register_endpoint(GET, "/index.html", index_html_handler);
+  register_endpoint(GET, "/", index_html_handler);
   http_server_netconn_init();
-  
+
   /* Infinite loop */
-  for(;;)
-  {
+  for (;;) {
     osDelay(0xFFFFFFFF);
   }
   /* USER CODE END StartDefaultTask */
@@ -130,7 +150,7 @@ void StartDefaultTask(void *argument)
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
-     
+
 /* USER CODE END Application */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
